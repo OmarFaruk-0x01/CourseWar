2af2333d399e612bdd2dfb17f6a651ec1c9cd783
Omar Faruk <programmer.omar.dev@gmail.com>
2022-05-20 03:01:45 +0600

    db.json file content add

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_URL: 'https://mocki.io/v1/6762917b-5d7c-45f6-b1d0-a571d84f661c'
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com']
