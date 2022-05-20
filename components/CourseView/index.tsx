93893f6c983c74bf4c028ec13c7c1c098711b02e
Omar Faruk <programmer.omar.dev@gmail.com>
2022-05-21 03:10:38 +0600

    Change Course View

--- components/CourseView/index.tsx
+++ components/CourseView/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import Image from "next/image";
 import { CourseShortTypes } from "../../constants/types";
-import { BiBox, BiBriefcaseAlt2, BiStar } from "react-icons/bi";
+import { BiBox, BiBriefcaseAlt2, BiPlayCircle, BiStar } from "react-icons/bi";
 import IconText from "../IconText";
 import { MdStar } from "react-icons/md";
 import Button from "../Button";
@@ -15,8 +15,15 @@ const CourseView: FC<CourseViewProps> = ({ course }) => {
   const { id, catagories, title, thumbnail, rating, level, description } =
     course;
   return (
-    <div className="w-full sm:max-w-lg desktop:max-w-xl bg-white shadow-lg rounded-lg overflow-hidden my-2">
-      <div className={"relative h-[250px] mobile:h-[300px] sm:!h-[250px]"}>
+    <div className=" hover:!bg-black group w-full sm:max-w-lg desktop:max-w-xl bg-white shadow-lg rounded-lg overflow-hidden my-2 ">
+      <div
+        className={
+          "relative h-[250px] mobile:h-[300px] sm:!h-[250px] mt-3 ml-3 mr-3 mb-0 rounded-lg overflow-hidden transition-all group-hover:bg-black"
+        }
+      >
+        <div className="z-50 absolute inset-[35%] bg-primary-50/80 rounded-full text-primary-400/30 shadow-xl flex items-center justify-center">
+          <BiPlayCircle size={60} />
+        </div>
         <Image src={thumbnail.url} layout="fill" />
       </div>
       {/* <div>
@@ -32,7 +39,9 @@ const CourseView: FC<CourseViewProps> = ({ course }) => {
       </div> */}
       <div className="flex  justify-between flex-col">
         <div className="px-4 flex-1">
-          <h2 className="text-lg font-semibold my-2">{title.slice(0,50)+' ......'}</h2>
+          <h2 className="text-lg font-semibold my-2">
+            {title.slice(0, 50) + " ......"}
+          </h2>
           <p className="text-sm my-3">{description.slice(0, 80) + " ..."}</p>
           <div className="flex  items-start justify-between gap-2 my-1">
             <IconText icon={<BiBriefcaseAlt2 size={16} />} title={level} />
--- components/CoursesView/index.tsx
+++ components/CoursesView/index.tsx
@@ -26,7 +26,7 @@ const CoursesView: FC<CoursesViewProps> = ({ courses }) => {
     );
   }
   return (
-    <div className="p-2  grid grid-cols-1  gap-2 justify-items-center tablet:grid-cols-2 tablet:!justify-items-start big_desktop:!grid-cols-4 desktop:!grid-cols-3 ">
+    <div className="p-2  grid grid-cols-1  gap-2 justify-items-center tablet:grid-cols-2 tablet:!justify-items-start big_desktop:!grid-cols-4 desktop:!grid-cols-3 desktop:!gap-5">
       {renderCourses()}
     </div>
   );
